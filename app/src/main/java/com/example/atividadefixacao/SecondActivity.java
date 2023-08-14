38e2c81a637055e5f6fde107af3a5a08d1e4345a
DAniel ByWhere <dan.bb2904@gmail.com>
2023-08-14 19:29:08 -0300

    Criação das Telas

---
+++ app/src/main/java/com/example/atividadefixacao/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.atividadefixacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+
+}
