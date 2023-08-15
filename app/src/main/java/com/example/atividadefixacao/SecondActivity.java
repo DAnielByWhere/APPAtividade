7cd2260de99b7574263844e49bfe595ac7de056b
DAniel ByWhere <dan.bb2904@gmail.com>
2023-08-15 20:27:20 -0300

    Adicionando as funcionalidades ao app

--- app/src/main/java/com/example/atividadefixacao/SecondActivity.java
+++ app/src/main/java/com/example/atividadefixacao/SecondActivity.java
@@ -2,16 +2,38 @@ package com.example.atividadefixacao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    private EditText editTextA, editTextB;
+    private TextView textViewResultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        editTextA = findViewById(R.id.editTextA);
+        editTextB = findViewById(R.id.editTextB);
+        textViewResultado = findViewById(R.id.textViewResultado);
     }
 
+    @SuppressLint("SetTextI18n")
+    public void verificarMultiplos(View view) {
+        int valorA = Integer.parseInt(editTextA.getText().toString());
+        int valorB = Integer.parseInt(editTextB.getText().toString());
 
+        if (valorA % valorB == 0 || valorB % valorA == 0) {
+            textViewResultado.setText("São múltiplos");
+        } else {
+            textViewResultado.setText("Não são múltiplos");
+        }
+    }
 }
+
--- app/src/main/java/com/example/atividadefixacao/ThirdActivity.java
+++ app/src/main/java/com/example/atividadefixacao/ThirdActivity.java
@@ -1,14 +1,61 @@
 package com.example.atividadefixacao;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
+
 
 public class ThirdActivity extends AppCompatActivity {
+    private EditText editTextOpcao, editTextQTD;
+    private TextView textViewPreco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        editTextQTD = findViewById(R.id.editTextQTD);
+        editTextOpcao = findViewById(R.id.editTextOpcao);
+        textViewPreco = findViewById(R.id.textViewPreco);
+
     }
+
+    @SuppressLint("SetTextI18n")
+    public void Preco (View view) {
+        int QTD = Integer.parseInt(editTextQTD.getText().toString());
+        double Valor;
+        int Preco;
+        String Opcao = editTextOpcao.getText().toString();
+
+        switch (Opcao) {
+            case "C":
+                Valor = 2;
+                Preco = (int) (Valor * QTD);
+
+                textViewPreco.setText("O valor é de " + Preco +" Reais");
+                break;
+            case "R":
+                Valor = 2.50;
+                Preco = (int) (Valor * QTD);
+
+                textViewPreco.setText("O valor é de " + Preco + " Reais");
+                break;
+            case "S":
+                Valor = 1.50;
+                Preco = (int) (Valor * QTD);
+
+                textViewPreco.setText("O valor é de" + Preco + " Reais");
+                break;
+            default:
+                textViewPreco.setText("Há algo errado, verifique as informações inseridas");
+                break;
+        }
+    }
+
+
 }
